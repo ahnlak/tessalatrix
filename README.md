cf55f8a27daa0f17a3d0f5c839bdc6dcb1e6c334
Pete Favelle <pete@fsquared.co.uk>
2023-01-05 09:47:21 +0000

    First layout commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tessalatrix
+
 A flexible cross platform version of Tetris, using C and SDL.
+
+This is primarily being developed to give me a way to explore the modern
+state of SDL, with a view to building something a little more ... original
+in the future.
+
+Ultimately, I aim to be able to target multiple platforms (Linux & Windows
+definitely, Web and even MacOS ideally) with a common code base.
+
+As always, this is all released under the MIT License.
---
+++ src/tessalatrix.c
@@ -0,0 +1,35 @@
+/*
+ * tessalatrix.c - part of Tessalatrix
+ *
+ * This is the main entry point of the game; you'll find the main() function
+ * here. Everything else should be neatly tucked away in their own files!
+ *
+ * Copyright (c) 2023 Pete Favelle <ahnlak@ahnlak.com>
+ *
+ * This file is released under the MIT License; see LICENSE for details.
+ */
+
+/* System headers. */
+
+#include <stdio.h>
+#include "SDL.h"
+
+
+/* Local headers. */
+
+#include "config.h"
+
+
+/* Functions. */
+
+/*
+ * main - the standard entry to the program.
+ */
+
+int main( int argc, char **argv )
+{
+  /* All done, return success to the commandline. */
+  return 0;
+}
+
+/* End of file tessalatrix.c */
