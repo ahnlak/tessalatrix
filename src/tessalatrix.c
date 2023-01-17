8c9385785f7beb4ddc47f7968d73bed8a379c8d6
Pete Favelle <pete@fsquared.co.uk>
2023-01-17 14:28:55 +0000

    Basic Emscripten compatability

--- src/tessalatrix.c
+++ src/tessalatrix.c
@@ -14,13 +14,86 @@
 #include <stdio.h>
 #include "SDL.h"
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
 
 /* Local headers. */
 
 #include "tessalatrix.h"
 
 
+/* Module variables. */
+
+static trix_engine_st  m_current_engine;
+static bool            m_running = true;
+
+
+
 /* Functions. */
+
+/*
+ * main_loop - the core application logic; split off here so that Emscripten
+ *             can be handed a simple loop to run, but works fine with other
+ *             platforms too.
+ */
+
+void main_loop( void )
+{
+  SDL_Event       l_event;
+  trix_engine_t   l_target_engine;
+
+  /* So, work through any queued up events. */
+  while( SDL_PollEvent( &l_event ) != 0 )
+  {
+    /* Handle the system-level events. */
+    if ( l_event.type == SDL_QUIT )
+    {
+      m_running = false;
+      break;
+    }
+
+    /* And pass any remaining events into the current engine. */
+    m_current_engine.event( &l_event );
+  }
+
+  /* If we've quit, we probably don't need to do any more! */
+  if ( !m_running )
+  {
+    return;
+  }
+
+  /* Ask the current engine to update. */
+  l_target_engine = m_current_engine.update();
+
+  /* If the engine has requested a switch, do so and move straight on. */
+  if ( l_target_engine != m_current_engine.type )
+  {
+    /* Tell the current engine to shut down. */
+    m_current_engine.fini();
+
+    /* Set up the current engine structure to point to the target. */
+    switch( l_target_engine )
+    {
+      case ENGINE_EXIT:       /* We want to exit the game now. */
+      default:
+        m_running = false;
+        break;
+    }
+
+    /* And move straight on with the next loop. */
+    return;
+  }
+
+  /* And then to render itself (if we have time) */
+  m_current_engine.render();
+
+  /* Wait for the next tick, if we need to. */
+
+  /* All done for this loop. */
+  return;
+}
+
 
 /*
  * main - the standard entry to the program.
@@ -28,11 +101,6 @@
 
 int main( int argc, char **argv )
 {
-  bool            l_running = true;
-  SDL_Event       l_event;
-  trix_engine_t   l_target_engine;
-  trix_engine_st  l_current_engine;
-
   /* Initialise our configuration. */
   if ( !config_load( argc, argv ) )
   {
@@ -49,69 +117,28 @@ int main( int argc, char **argv )
   log_write( ALWAYS, "%s started.", util_app_namever() );
 
   /* Set up the initial engine. */
-  l_current_engine.type = ENGINE_SPLASH;
-  l_current_engine.init = splash_init;
-  l_current_engine.event = splash_event;
-  l_current_engine.update = splash_update;
-  l_current_engine.render = splash_render;
-  l_current_engine.fini = splash_fini;
+  m_current_engine.type = ENGINE_SPLASH;
+  m_current_engine.init = splash_init;
+  m_current_engine.event = splash_event;
+  m_current_engine.update = splash_update;
+  m_current_engine.render = splash_render;
+  m_current_engine.fini = splash_fini;
 
   /* Set up the display. */
   if ( display_init() )
   {
     /* Initialise the starting engine (display needs to be initialised first) */
-    l_current_engine.init();
+    m_current_engine.init();
 
     /* Dive into the main logic loop, until it exists. */
-    while( l_running )
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop( main_loop, 0, 1 );
+#else
+    while( m_running )
     {
-      /* So, work through any queued up events. */
-      while( SDL_PollEvent( &l_event ) != 0 )
-      {
-        /* Handle the system-level events. */
-        if ( l_event.type == SDL_QUIT )
-        {
-          l_running = false;
-          break;
-        }
-
-        /* And pass any remaining events into the current engine. */
-        l_current_engine.event( &l_event );
-      }
-
-      /* If we've quit, we probably don't need to do any more! */
-      if ( !l_running )
-      {
-        break;
-      }
-
-      /* Ask the current engine to update. */
-      l_target_engine = l_current_engine.update();
-
-      /* If the engine has requested a switch, do so and move straight on. */
-      if ( l_target_engine != l_current_engine.type )
-      {
-        /* Tell the current engine to shut down. */
-        l_current_engine.fini();
-
-        /* Set up the current engine structure to point to the target. */
-        switch( l_target_engine )
-        {
-          case ENGINE_EXIT:       /* We want to exit the game now. */
-          default:
-            l_running = false;
-            break;
-        }
-
-        /* And move straight on with the next loop. */
-        continue;
-      }
-
-      /* And then to render itself (if we have time) */
-      l_current_engine.render();
-
-      /* Wait for the next tick, if we need to. */
+      main_loop();
     }
+#endif
 
     /* Lastly, tear down the display. */
     display_fini();
